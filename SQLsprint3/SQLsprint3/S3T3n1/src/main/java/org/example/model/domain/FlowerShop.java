e4535e8a40d7b5aed4e5bb67608da34c933aac16
ArnauBurguera <55574947+ArnauBurguera@users.noreply.github.com>
2023-10-02 21:45:48 +0200

    Add files via upload

---
+++ SQLsprint3/SQLsprint3/S3T3n1/src/main/java/org/example/model/domain/FlowerShop.java
@@ -0,0 +1,117 @@
+package org.example.model.domain;
+
+import org.example.model.domain.entity.Product;
+import org.example.model.repository.DBInteraction;
+import org.example.model.repository.DatabaseConnection;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FlowerShop {
+    private String name;
+    private List<Product> stockList;
+    private List<Invoice> invoiceLog;
+    private double totalEarnings;
+    private double stockTotalValue;
+    private Map<String,Integer> productMap;
+
+    public FlowerShop(String name) {
+        this.name = name;
+        this.stockList = new ArrayList<>();
+        this.invoiceLog = new ArrayList<>();
+        this.totalEarnings = 0.0d;
+        this.stockTotalValue = 0.0d;
+        this.productMap = new HashMap<>();
+        createMap();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Product> getStockList() {
+        return stockList;
+    }
+
+    public void setStockList(List<Product> stockList) {
+        this.stockList = stockList;
+    }
+
+    public List<Invoice> getInvoiceLog() {
+        return invoiceLog;
+    }
+
+    public void setInvoiceLog(List<Invoice> invoiceLog) {
+        this.invoiceLog = invoiceLog;
+    }
+
+    public double getTotalEarnings() {
+        return totalEarnings;
+    }
+
+    public void setTotalEarnings(double totalEarnings) {
+        this.totalEarnings = totalEarnings;
+    }
+
+    public double getStockTotalValue() {
+        return stockTotalValue;
+    }
+
+    public void setStockTotalValue(double stockTotalValue) {
+        this.stockTotalValue = stockTotalValue;
+    }
+
+    public Map<String, Integer> getProductMap() {
+        return productMap;
+    }
+
+    public void setProductMap(Map<String, Integer> productMap) {
+        this.productMap = productMap;
+    }
+
+    public void createMap(){
+        productMap.put("Tree",0);
+        productMap.put("Flower",0);
+        productMap.put("Decoration",0);
+    }
+
+    public void addStock(Product p){
+        stockList.add(p);
+        stockTotalValue += p.getPrice();
+        productMap.put(p.getType(), (int)productMap.get(p.getType()) + 1);
+    }
+
+    public void removeStock(Product p){
+        stockList.remove(p);
+        stockTotalValue -= p.getPrice();
+        productMap.put(p.getType(), (int)productMap.get(p.getType()) - 1);
+    }
+
+    public void showStock(){
+        stockList.forEach(System.out::println);
+        System.out.print("\n");
+    }
+
+    public void addInvoice(Invoice i ){
+        invoiceLog.add(i);
+        totalEarnings += i.getTotalSale();
+    }
+
+    public void removeInvoice(Invoice i){
+        if(invoiceLog.contains(i)){
+            invoiceLog.remove(i);
+            totalEarnings -= i.getTotalSale();
+        }
+    }
+
+    public void showInvoiceList() {
+        invoiceLog.forEach(System.out::println);
+    }
+
+}
---
+++ SQLsprint3/SQLsprint3/S3T3n1/src/main/java/org/example/model/repository/DBInteraction.java
@@ -0,0 +1,117 @@
+package org.example.model.repository;
+
+import org.example.model.domain.Invoice;
+import org.example.model.domain.entity.Decoration;
+import org.example.model.domain.entity.Flower;
+import org.example.model.domain.entity.Product;
+import org.example.model.domain.entity.Tree;
+import org.example.model.repository.interfaces.InvoiceDAO;
+import org.example.model.repository.invoice.InvoiceDAOImpl;
+import org.example.model.repository.invoice.InvoicesTableInitialization;
+import org.example.model.repository.stock.StockSQLServerDAO;
+import org.example.model.repository.stock.StockTableInitialization;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class DBInteraction implements org.example.model.repository.interfaces.DBInteraction {
+
+    private static StockSQLServerDAO stockDAO = new StockSQLServerDAO();
+    private static InvoiceDAOImpl invoiceDAO = new InvoiceDAOImpl();
+
+
+    @Override
+    public boolean stockDataIsEmpty() {
+        return stockDAO.findAll().isEmpty();
+    }
+
+    @Override
+    public boolean invoiceDataIsEmpty() {
+        return invoiceDAO.invoicesRetriever().isEmpty();
+    }
+
+    @Override
+    public List<Product> getStockData() {
+        List<Product> stockList = new ArrayList<>();
+
+        for(Product p : stockDAO.findAll()){
+            if(p.getInvoiceId() == 0){
+                stockList.add(p);
+            }
+        }
+        return stockList;
+    }
+
+    @Override
+    public List<Product> getProductsSoldData() {
+        List<Product> soldProductsList = new ArrayList<>();
+
+        for(Product p : stockDAO.findAll()){
+            if(p.getInvoiceId() != 0){
+                soldProductsList.add(p);
+            }
+        }
+        return soldProductsList;
+    }
+
+    @Override
+    public List<Invoice> getInvoiceData() {
+        return invoiceDAO.invoicesRetriever();
+    }
+
+    @Override
+    public void insertTree(Product tree) {
+        stockDAO.insertProduct(tree);
+    }
+
+    @Override
+    public void insertFlower(Product flower) {
+        stockDAO.insertProduct(flower);
+    }
+
+    @Override
+    public void insertDecoration(Product decoration) {
+        stockDAO.insertProduct(decoration);
+    }
+
+    @Override
+    public void removeTree(Product tree) {
+        stockDAO.deleteProduct(tree.getId());
+    }
+
+    @Override
+    public void removeFlower(Product flower) {
+        stockDAO.deleteProduct(flower.getId());
+    }
+
+    @Override
+    public void removeDecoration(Product decoration) {
+        stockDAO.deleteProduct(decoration.getId());
+    }
+
+    @Override
+    public Map<String, Integer> getProductMapData() {
+        return null;
+    }//Not sure if we need it if we compile stocklist in flowershop class product by product
+
+    @Override
+    public void insertInvoice(Invoice invoice) {
+        invoiceDAO.insertInvoice(invoice);
+    }
+
+    @Override
+    public void tablesInitializer() {
+        InvoicesTableInitialization.createTable();
+        StockTableInitialization.createTable();
+    }
+
+    @Override
+    public void updateProduct(Product product) {
+        stockDAO.updateProduct(product);
+    }
+
+    public void updateInvocie(Invoice invoice){
+        invoiceDAO.updateInvoice(invoice);
+    }
+}
